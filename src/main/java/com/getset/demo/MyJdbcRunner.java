48a3f47a23a2b8b9a630af348388c14ec85db59a
ramakrishna <ramakrishna@LAPTOP-QSONDSEP>
2020-05-01 16:43:39 +0530

    first commit by tl

---
+++ src/main/java/com/getset/demo/MyJdbcRunner.java
@@ -0,0 +1,14 @@
+package com.getset.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class MyJdbcRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+System.out.println("myjdbc runner starts....");
+	}
+
+}
---
+++ src/main/java/com/getset/demo/SpringbootrunnerstwoApplication.java
@@ -0,0 +1,13 @@
+package com.getset.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrunnerstwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrunnerstwoApplication.class, args);
+	}
+
+}
